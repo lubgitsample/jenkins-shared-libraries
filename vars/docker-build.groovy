b6517829b1cf3507171715889d5f8e0c8beea078
lubgitsample <udaipurwala.lubna@gmail.com>
2025-05-21 23:29:56 +0530

    Update docker-build.groovy

--- vars/docker-build.groovy
+++ vars/docker-build.groovy
@@ -1,3 +1,3 @@
-def call(String ProjectName,String ImageTag,String dockerHubUser){
-  sh " docker build -t ${dockerHubUser}/${dockerHubUser}:${ImageTag} ."
+def call(String ProjectName, String ImageTag, String dockerHubUser) {
+  sh "docker build -t ${dockerHubUser}/${ProjectName}:${ImageTag} ."
 }
