0fd45442d275d80c676bbd2e83425569e501fc7e
lubgitsample <udaipurwala.lubna@gmail.com>
2025-05-21 23:33:27 +0530

    Update docker-push.groovy

--- vars/docker-push.groovy
+++ vars/docker-push.groovy
@@ -1,10 +1,11 @@
-def call(String ProjectName,String ImageTag,String dockerHubUser){
+def call(String ProjectName, String ImageTag, String dockerHubUser){
   withCredentials([usernamePassword(
-                    'credentialsId':"dockerhubcred",
-                    passwordVariable:"dockerHubPass",
-                    usernameVariable:"dockerHubUser")]){
-                sh " docker login -u ${dockerHubUser} -p ${dockerHubPass}"
-                sh " docker image tag flask-web-app:latest ${dockerHubUser}/flask-web-app:latest "
-                sh "docker push ${dockerHubUser}/flask-web-app:latest"
-               }
+    credentialsId: "dockerhubcred",
+    passwordVariable: "dockerHubPass",
+    usernameVariable: "dockerHubUser"
+  )]){
+    sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+    sh "docker image tag ${ProjectName}:latest ${dockerHubUser}/${ProjectName}:${ImageTag}"
+    sh "docker push ${dockerHubUser}/${ProjectName}:${ImageTag}"
+  }
 }
